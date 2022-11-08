a3ee87930cb93959bbf191db903dbc86bb3e3b10
angelxtry <angelxtry@gmail.com>
2022-11-08 18:39:12 +0900

    tailwindcss에 GDS 일부 적용

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -9,6 +9,7 @@ const HomePage = () => (
         <p className='mb-5'>글로벌 이커머스 플랫폼 키워드 분석도구</p>
         <h1 className='mb-5 text-5xl font-bold'>GOMI 키워드</h1>
         <button className='btn-primary btn'>Get Started</button>
+        <div className='text-600-bold text-primary-red-orange'>테스트</div>
       </div>
     </div>
   </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,39 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
-  plugins: [require('@tailwindcss/line-clamp'), require("daisyui")],
+  plugins: [require('@tailwindcss/line-clamp'), require('daisyui')],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          'red-orange': '#FF5100',
+          black: '#333333',
+        },
+        grey: {
+          400: '#D9D9D9',
+          600: '#A8A8A8',
+          900: '#262626',
+        },
+        functional: {
+          success: '#72C040',
+        },
+      },
+      fontSize: {
+        '100-medium': ['10px', { fontWeight: 500, lineHeight: '160%' }],
+
+        '200-regular': ['13px', { fontWeight: 400, lineHeight: '150%' }],
+        '200-medium': ['13px', { fontWeight: 500, lineHeight: '150%' }],
+
+        '300-regular': ['15px', { fontWeight: 400, lineHeight: '150%' }],
+        '300-medium': ['15px', { fontWeight: 500, lineHeight: '150%' }],
+
+        '400-medium': ['17px', { fontWeight: 500, lineHeight: '150%' }],
+
+        '500-medium': ['20px', { fontWeight: 500, lineHeight: '150%' }],
+        '500-bold': ['20px', { fontWeight: 700, lineHeight: '150%' }],
+
+        '600-medium': ['24px', { fontWeight: 500, lineHeight: '150%' }],
+        '600-bold': ['24px', { fontWeight: 700, lineHeight: '150%' }],
+      },
+    },
+  },
 };
