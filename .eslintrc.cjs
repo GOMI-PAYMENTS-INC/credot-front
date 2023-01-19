37e5c589ff2eb00ffd50c05c9964b43eaacb666c
Tate <tate@gomicorp.com>
2023-01-19 10:02:25 +0900

    fix: 잘못된 path name으로 인한 이슈

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -50,7 +50,7 @@ module.exports = {
       files: '**/*.+(ts|tsx)',
       parser: '@typescript-eslint/parser',
       parserOptions: {
-        project: ['./tsconfig/json', './apps/**/tsconfig.json'],
+        project: ['./tsconfig.json', './apps/**/tsconfig.json'],
       },
       plugins: ['@typescript-eslint'],
       extends: ['airbnb-base', 'airbnb-typescript/base'],
