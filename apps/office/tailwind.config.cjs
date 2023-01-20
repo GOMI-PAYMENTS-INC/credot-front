704ffce97c25919c69411b842b72a480d0047117
hoyashu <thwls0807@naver.com>
2023-01-20 11:43:17 +0900

    fix: tailwind config root에서 적용 건 오류 수정, 구글로그인 버튼 수정

--- apps/office/tailwind.config.cjs
+++ apps/office/tailwind.config.cjs
@@ -3,10 +3,32 @@ const tailwindConfig = require('../../tailwind.config.cjs');
 module.exports = {
   ...tailwindConfig,
   content: [`./src/**/*.{html,js,jsx,ts,tsx}`],
-  extend: {
-    ...tailwindConfig.extend,
-    backgroundImage: {
-      main: "url('/src/assets/images/Section8/Background.png')",
+  theme: {
+    ...tailwindConfig.theme,
+
+    extend: {
+      ...tailwindConfig.theme.extend,
+
+      backgroundImage: {
+        main: "url('./src/assets/images/Section8/Background.png')",
+      },
+
+      gridTemplateColumns: {
+        //선택한 키워드 아이템 내부 grid
+        'select-keyword': '0.23fr 1fr 0.38fr',
+
+        'tab': 'repeat(5, minmax(140px, 1fr))',
+      },
+
+      boxShadow: {
+        'partner-card': '0px 10.1598px 20.3197px rgba(0, 0, 0, 0.08)',
+      },
+
+      gap: {
+        //선택한 키워드 아이템 내부 grid gap
+        'select-keyword-item': '0.6875rem',
+      },
     },
-  },
+  }
+
 };
--- apps/user/tailwind.config.cjs
+++ apps/user/tailwind.config.cjs
@@ -1,118 +1,13 @@
+const tailwindConfig = require('../../tailwind.config.cjs');
+
 module.exports = {
-  content: [ './src/**/*.{html,js,jsx,ts,tsx}' ],
-  //line-clamp 사용법 : https://postsrc.com/code-snippets/how-to-multi-line-trucate-text-in-tailwindcss
-  plugins: [ require('@tailwindcss/line-clamp'), require('daisyui') ],
-  daisyui: { themes: false },
+  ...tailwindConfig,
+  content: [`./src/**/*.{html,js,jsx,ts,tsx}`],
   theme: {
-
-    screens: {
-      'xl': {'max': '1440px'},
-      'lg': {'max': '992px'},
-      'md': {'max': '768px'},
-      'sm': {'max': '576px'},
-      'xs': {'max': '431px'},
-    },
-
-    container: {
-      center: true,
-
-      padding: {
-      },
-
-      screens: {
-        'xl': '1320px',
-      },
-    },
+    ...tailwindConfig.theme,
 
     extend: {
-
-      //색상
-      colors: {
-        primary: {
-          'red-orange': '#FF5100',
-        },
-
-        orange:{
-          900: '#9E2F00',
-          800: '#B83A00',
-          700: '#D54400',
-          600: '#E64900',
-          500: '#FF5100',
-          400: '#FF6C28',
-          300: '#FFA378',
-          200: '#FFDAC8',
-          100: '#FFF5F0',
-          60: '#FFF8F5'
-        },
-
-        grey: {
-          50: '#FCFCFC',
-          100: '#F8F8F8',
-          200: '#F5F5F5',
-          300: '#EBEBEB',
-          400: '#D9D9D9',
-          500: '#BFBFBF',
-          600: '#A8A8A8',
-          700: '#8C8C8C',
-          800: '#595959',
-          900: '#262626',
-        },
-        //text-functional-success, text-functional-error
-        functional: {
-          link: '#4090F7',
-          success: '#72C040',
-          warning: '#EFAF41',
-        },
-      },
-
-      //폰트 설정
-      fontSize: {
-        '4XL/Bold': [' 48px', { fontWeight: 700, lineHeight: '60px' }],
-        '4XL/Medium': ['48px', { fontWeight: 500, lineHeight: '60px' }],
-        '4XL/Regular': ['48px', { fontWeight: 400, lineHeight: '60px' }],
-        '4XL/Light': ['48px', { fontWeight: 400, lineHeight: '60px' }],
-
-        '3XL/Bold': ['32px', { fontWeight: 700, lineHeight: '40px' }],
-        '3XL/medium': ['32px', { fontWeight: 500, lineHeight: '40px' }],
-        '3XL/Regular': ['32px', { fontWeight: 400, lineHeight: '40px' }],
-        '3XL/Light': ['32px', { fontWeight: 400, lineHeight: '40px' }],
-
-        '2XL/Bold': ['24px', { fontWeight: 700, lineHeight: '32px' }],
-        '2XL/Medium': ['24px', { fontWeight: 500, lineHeight: '32px' }],
-        '2XL/Regular': ['24px', { fontWeight: 400, lineHeight: '32px' }],
-        '2XL/Light': ['24px', { fontWeight: 400, lineHeight: '32px' }],
-
-        'XL/Bold': ['20px', { fontWeight: 700, lineHeight: '28px' }],
-        'XL/Medium': ['20px', { fontWeight: 500, lineHeight: '28px' }],
-        'XL/Regular': ['20px', { fontWeight: 400, lineHeight: '28px' }],
-        'XL/Light': ['20px', { fontWeight: 300, lineHeight: '28px' }],
-
-        'L/Bold': ['18px', { fontWeight: 700, lineHeight: '26px' }],
-        'L/Medium': ['18px', { fontWeight: 500, lineHeight: '26px' }],
-        'L/Regular': ['18px', { fontWeight: 400, lineHeight: '26px' }],
-        'L/Light': ['18px', { fontWeight: 300, lineHeight: '26px' }],
-
-        'M/Bold': ['16px', { fontWeight: 700, lineHeight: '24px' }],
-        'M/Medium': ['16px', { fontWeight: 500, lineHeight: '24px' }],
-        'M/Regular': ['16px', { fontWeight: 400, lineHeight: '24px' }],
-        'M/Light': ['16px', { fontWeight: 300, lineHeight: '24px' }],
-
-        'S/Bold': ['14px', { fontWeight: 700, lineHeight: '24px' }],
-        'S/Medium': ['14px', { fontWeight: 500, lineHeight: '24px' }],
-        'S/Regular': ['14px', { fontWeight: 400, lineHeight: '24px' }],
-        'S/Light': ['14px', { fontWeight: 300, lineHeight: '24px' }],
-
-        'XS/Bold': ['12px', { fontWeight: 700, lineHeight: '16px' }],
-        'XS/Medium': ['12px', { fontWeight: 500, lineHeight: '16px' }],
-        'XS/Regular': ['12px', { fontWeight: 400, lineHeight: '16px' }],
-        'XS/Light': ['12px', { fontWeight: 300, lineHeight: '16px' }],
-
-        '2XS/Bold': ['10px', { fontWeight: 700, lineHeight: '12px' }],
-        '2XS/Medium': ['10px', { fontWeight: 500, lineHeight: '12px' }],
-        '2XS/Regular': ['10px', { fontWeight: 400, lineHeight: '12px' }],
-        '2XS/Light': ['10px', { fontWeight: 300, lineHeight: '12px' }],
-
-      },
-    },
-  },
+      ...tailwindConfig.theme.extend,
+      }
+  }
 };
