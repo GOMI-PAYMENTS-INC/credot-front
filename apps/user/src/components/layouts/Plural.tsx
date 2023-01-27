8552a2ce4826a48fbcaa4a843a9efe4ef26090b1
Tate <tate@gomicorp.com>
2023-01-27 16:50:42 +0900

    chore: 불필요한 파일, 로직 제거

--- apps/user/src/components/layouts/Plural.tsx
+++ apps/user/src/components/layouts/Plural.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from 'react';
 
 export interface IPluralProps {
-  //   leftNode: ReactNode;
-  //   rightNode: ReactNode;
   children?: ReactNode;
 }
-// const Plural: FC<IPluralProps> = ({ leftNode, rightNode }) => (
+
 export const Plural = ({ children }: IPluralProps) => (
   <div className='container'>
     <div className='grid min-w-[1320px] grid-cols-12 gap-x-6'>{children}</div>
