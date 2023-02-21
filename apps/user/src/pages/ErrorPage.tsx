a395927ec56f468472675387f21c857c5a7050df
Tate <tate@gomicorp.com>
2023-02-21 16:37:17 +0900

    feat: error page 추가

---
+++ apps/user/src/pages/ErrorPage.tsx
@@ -0,0 +1,38 @@
+export const ErrorPage = () => {
+  const href = window.location.href;
+
+  return (
+    <div className='flex h-full w-full flex-col items-center justify-center text-center'>
+      <div>
+        <img src='/assets/images/ErrorPage.png' />
+      </div>
+      <div className='mt-7'>
+        <h1 className='text-2XL/Bold'>페이지를 정상적으로 불러오지 못했어요.</h1>
+        <p className='mt-6 text-M/Regular'>
+          페이지를 연결하는 과정에서 저희측 문제가 있는 것 같아요. 재시도 부탁드리며,
+          <br />이 문제가 계속해서 발생하는 경우
+          <span className='text-orange-500'>{` kai@gomiinsight.com`}</span>로 문의주세요.
+        </p>
+      </div>
+      <div className='mt-14 space-x-6'>
+        <button
+          className='button-filled-normal-large-primary-false-false-true h-12 w-[200px] bg-grey-200'
+          onClick={async () => {
+            window.history.back();
+            setTimeout(() => {
+              window.location.reload();
+            }, 100);
+          }}
+        >
+          <p className='text-M/Bold text-grey-800'>이전 화면으로</p>
+        </button>
+        <button
+          className='button-filled-normal-large-primary-false-false-true h-12 w-[200px] bg-orange-500 text-center'
+          onClick={() => window.location.replace(href)}
+        >
+          <p className='text-M/Bold text-white'>재시도</p>
+        </button>
+      </div>
+    </div>
+  );
+};
