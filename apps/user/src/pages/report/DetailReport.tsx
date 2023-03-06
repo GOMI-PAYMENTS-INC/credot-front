a5c522c4412872298beed9a109afd50b3cb30c3f
Tate <tate@gomicorp.com>
2023-03-06 19:21:06 +0900

    fix: 가격경쟁률 순서 이상 수정

--- apps/user/src/pages/report/DetailReport.tsx
+++ apps/user/src/pages/report/DetailReport.tsx
@@ -101,11 +101,6 @@ const DetailReport = () => {
                 </div>
               )}
             </div>
-            {_state.scrollEvent.title !== TITLE.REPORT && (
-              <div className='mr-2.5  w-[120px] rounded border-[1px] border-grey-300 text-center'>
-                <p className='py-2.5 text-S/Regular'>상위 30개</p>
-              </div>
-            )}
           </div>
         </div>
       </header>
