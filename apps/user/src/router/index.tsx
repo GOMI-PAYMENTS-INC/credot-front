20590934a666960e6969fd8a3a8c1cdb5292e2df
hoyashu <thwls0807@naver.com>
2023-03-24 13:58:38 +0900

    feat: amplitude 리포트 이벤트 생성

--- apps/user/src/router/index.tsx
+++ apps/user/src/router/index.tsx
@@ -6,10 +6,10 @@ import { routeList } from '@/router/routeList';
 export const Router = () => {
   return (
     <Routes>
-      {routeList.map((route) => {
+      {routeList.map((route, index) => {
         if (route.isPrivate) {
           return (
-            <Route element={<PrivateRoute />}>
+            <Route key={index} element={<PrivateRoute />}>
               {/* 인증을 반드시 해야지만 접속 가능한 페이지 정의 */}
               <Route path={route.path} element={createElement(route.component)} />
             </Route>
