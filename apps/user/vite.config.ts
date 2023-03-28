72def1ad26a11bef4e0d735e7b4976b7560490cf
hoyashu <thwls0807@naver.com>
2023-03-28 17:26:46 +0900

    fix: 개발/운영 서버에 따라 api key다르게 적용되도록 함

--- apps/user/vite.config.ts
+++ apps/user/vite.config.ts
@@ -2,10 +2,18 @@ import react from '@vitejs/plugin-react';
 import * as path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePluginHtmlEnv(),
+    VitePluginHtmlEnv({
+      compiler: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -16,7 +24,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       '/': {
-        target: 'https://gomi-insight.gomicorp.com/',
+        target: 'https://app.gomiinsight.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\//, ''),
       },
