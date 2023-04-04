077a4be83b05acffe9b8b46ffc6bdb5f7f3def20
Tate <tate@gomicorp.com>
2023-04-04 14:00:12 +0900

    fix: 에러 발생 시 화면 컴포넌트

--- apps/user/src/containers/search/translator.container.ts
+++ apps/user/src/containers/search/translator.container.ts
@@ -90,8 +90,7 @@ export const switchIsLoadingState = (
 export const getTranslatorStatus = (translatorState: TTranslationKeywordType) => {
   if (isFalsy(translatorState.keyword) && isFalsy(translatorState.isLoading))
     return SEARCH_KEYWORD_STATUS.LANDING;
-  if (isFalsy(translatorState.data) && translatorState.isError)
-    return SEARCH_KEYWORD_STATUS.NONE_DATA_ERROR;
+  if (translatorState.isError) return SEARCH_KEYWORD_STATUS.NONE_DATA_ERROR;
   if (translatorState.isLoading || isFalsy(translatorState.data?.dictionaries))
     return SEARCH_KEYWORD_STATUS.NONE_DATA_LOADING;
   return SEARCH_KEYWORD_STATUS.SUCCESS;
