c88dc3e8fd8c50b2ffb15795f3785ac8dcf59194
hoyashu <thwls0807@naver.com>
2023-06-09 01:52:36 +0900

    fix: 쿠키 삭제 안되던 버그 수정 (path추가)

--- apps/user/src/utils/useCookieStorage.ts
+++ apps/user/src/utils/useCookieStorage.ts
@@ -17,6 +17,6 @@ export const useCookieStorage = {
   },
 
   removeCookie: (name: string) => {
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
   },
 };
