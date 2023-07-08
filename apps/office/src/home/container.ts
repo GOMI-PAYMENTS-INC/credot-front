64674a2ffcdd0a6215026a721c809b93ee7b99e3
tate <jun931223@gmail.com>
2023-07-08 12:35:27 +0900

    refactor: 렌딩페이지의 FAQ common 컴포넌트로 변경

--- apps/office/src/home/container.ts
+++ apps/office/src/home/container.ts
@@ -12,22 +12,6 @@ export const onChangeTab = (params: {
   swiper?.slideTo(tabIndex);
 };
 
-export const openFAQ = (params: {
-  faqIndex: number;
-  openedFAQ: number[];
-  setOpenedFAQ: Dispatch<SetStateAction<number[]>>;
-}) => {
-  const { faqIndex, openedFAQ, setOpenedFAQ } = params;
-  if (openedFAQ.find((one) => one === faqIndex)) {
-    //체크 해제할때 checkedItems에 있을 경우
-    const payload = openedFAQ.filter((one) => one !== faqIndex);
-    setOpenedFAQ(payload);
-  } else {
-    const payload = openedFAQ.concat(faqIndex);
-    setOpenedFAQ(payload);
-  }
-};
-
 //탭 이동 네비게이션-다음
 export const dragTab = (params: {
   activeTabIndex: number;
