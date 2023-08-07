49eef57413eb8cb7f36ad1acdd1c06869e5c2fe1
jun931223 <jun931223@gmail.com>
2023-08-08 07:47:03 +0900

    feat: 미리보기 배너 및 문구 변경

--- apps/office/src/preview/elements/PreviewFooter.tsx
+++ apps/office/src/preview/elements/PreviewFooter.tsx
@@ -10,19 +10,20 @@ import { Fragment } from 'react';
 export const PreviewFooter = () => {
   return (
     <Fragment>
-      <section className='mt-[50px] hidden h-[540px] xs:block'>
-        <div className='flex justify-center'>
+      <section className='mt-[50px] hidden h-[450px] xs:block'>
+        <div className='flex flex-col justify-center'>
           <div className='relative flex flex-col items-center'>
-            <div className='absolute mt-[50px] h-[335px] w-full bg-orange-500 opacity-20 blur-[132px]' />
+            <div className='absolute mt-[50px] h-[200px] w-full bg-orange-500 opacity-20 blur-[132px]' />
             <div className='flex h-full flex-col items-center justify-center text-L/Medium '>
-              <div className='flex w-[252px] flex-col justify-center break-words text-center leading-loose'>
-                <p>고미인사이트의 키워드 리포트는 Shopee 키워드 검색결과 화면에서</p>
-                <p className='mt-1'>
-                  광고 상품을 제외한 상위 50개의 상품 데이터를 기반으로 제공되어요.
+              <div className='flex w-[252px] flex-col justify-center break-words text-center text-XL/Medium'>
+                <p>회원가입 시 원하는 키워드를</p>
+                <p className=''>
+                  <span className='text-XL/Bold text-orange-400'>무제한</span>으로 분석할
+                  수 있어요!
                 </p>
               </div>
               <button
-                className='button-filled-xLarge-primary-false-false-false mt-[26px] w-fit text-L/Bold'
+                className='button-filled-xLarge-primary-false-false-false mt-5 w-fit text-L/Bold'
                 id='movedToSolution'
                 onClick={(event) => {
                   openAppWithTag({
@@ -36,13 +37,10 @@ export const PreviewFooter = () => {
               >
                 내가 원하는 키워드 분석하러 가기
               </button>
-
-              <ReactSVG
-                className='z-10'
-                src='/assets/icons/ReportL.svg'
-                beforeInjection={(svg) => svg.setAttribute('class', 'w-[360px]')}
-              />
             </div>
+          </div>
+          <div className='z-10 mt-8 flex h-full'>
+            <img src='/assets/images/MPreviewBanner.png' />
           </div>
         </div>
       </section>
@@ -51,10 +49,10 @@ export const PreviewFooter = () => {
         <div className='flex h-[340px] justify-center'>
           <div className='relative flex w-full justify-around overflow-hidden'>
             <div className='absolute ml-[500px] mt-[50px] h-[1000px] w-[1000px] rounded-[1000px] bg-orange-500 opacity-20 blur-[132px]' />
-            <div className='flex h-full flex-col justify-center text-XL/Bold'>
-              <p>고미인사이트의 키워드 리포트는 Shopee 키워드 검색결과 화면에서</p>
+            <div className='flex h-full flex-col justify-center text-3XL/Medium'>
+              <p>회원가입 시 원하는 키워드를</p>
               <p className='mt-1'>
-                광고 상품을 제외한 상위 50개의 상품 데이터를 기반으로 제공되어요.
+                <span className='text-orange-400'>무제한</span>으로 분석할 수 있어요!
               </p>
               <button
                 className='button-filled-xLarge-primary-false-false-false mt-[26px] w-fit text-L/Bold'
@@ -72,7 +70,10 @@ export const PreviewFooter = () => {
                 내가 원하는 키워드 분석하러 가기
               </button>
             </div>
-            <ReactSVG className='z-10 self-end' src='/assets/icons/ReportL.svg' />
+            <img
+              className='z-10 h-[318px] w-[724px] self-end'
+              src='/assets/images/PreviewBanner.png'
+            />
           </div>
         </div>
       </section>
