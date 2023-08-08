3710189ad6ea76d3ede98f570ab61c32d16b689f
jun931223 <jun931223@gmail.com>
2023-08-08 19:09:13 +0900

    feat: 구매전환율 계산식 변경

--- apps/office/src/preview/container.ts
+++ apps/office/src/preview/container.ts
@@ -250,10 +250,10 @@ export const getConversionRate = (rate: number) => {
   if (rate >= 0.3 && rate < 0.5) {
     return 'D';
   }
-  if (rate >= 0.5 && rate < 1) {
+  if (rate >= 0.5 && rate < 1.5) {
     return 'C';
   }
-  if (rate >= 1 && rate < 1.5) {
+  if (rate >= 1.5 && rate < 3) {
     return 'B';
   }
 
