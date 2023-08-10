5ac3da65dcaf524142d0fe1ce3ce3c35be48cb50
tate <jun931223@gmail.com>
2023-08-10 13:16:14 +0900

    feat: 모바일일 경우 링크는 텍스트 옆에

--- apps/user/src/report/keyword/RelativeKeywordTable.tsx
+++ apps/user/src/report/keyword/RelativeKeywordTable.tsx
@@ -20,6 +20,7 @@ import { openBrowser } from '@/utils/openBrowser';
 import { getConversionRate } from '@/report/keyword/container';
 import { convertToWon } from '@/report/keyword/container';
 import { getElementLocation } from '@/utils/getElementLocation';
+import { replaceOverLength } from '@/utils/replaceOverLength';
 
 interface IRecommendationChart {
   relations: TRelationReport[] | null;
@@ -112,12 +113,42 @@ export const RelativeKeywordTable = (props: IRecommendationChart) => {
                   <div
                     className={`flex justify-between border-[1px] border-grey-300 bg-grey-50 py-[18px] px-[15px] text-M/Medium ${backgroundColor} cursor-pointer hover:bg-grey-300`}
                   >
-                    {item.text}
+                    <p className='xs:hidden'>{item.text}</p>
+
+                    <div className='hidden items-center xs:flex'>
+                      {replaceOverLength(item.text, 30)}
+                      <button
+                        id='relative_linkout'
+                        className='z-20 ml-2 h-5 w-5 cursor-pointer items-center'
+                        onClick={() => {
+                          openBrowser(
+                            `${convertShopeeSiteUrl(country!)}/search?keyword=${
+                              item.text
+                            }`,
+                            sorted,
+                          );
+                          amplitudeData &&
+                            _amplitudeMovedToSERP(
+                              amplitudeData.param,
+                              amplitudeData.keyword,
+                              item.text,
+                            );
+                        }}
+                      >
+                        <ReactSVG
+                          className=''
+                          src='/assets/icons/outlined/Linkout.svg'
+                          beforeInjection={(svg) =>
+                            svg.setAttribute('class', 'fill-grey-900')
+                          }
+                        />
+                      </button>
+                    </div>
 
                     <div className='flex items-center'>
                       <button
                         id='relative_linkout'
-                        className='z-20 flex h-5 w-5 cursor-pointer items-center'
+                        className='z-20 flex h-5 w-5 cursor-pointer items-center xs:hidden'
                         onClick={() => {
                           openBrowser(
                             `${convertShopeeSiteUrl(country!)}/search?keyword=${
