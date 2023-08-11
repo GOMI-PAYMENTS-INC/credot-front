02bdd285807c4a41c12afd95b424bbd66de1cfcb
tate <jun931223@gmail.com>
2023-08-12 01:24:41 +0900

    feat: 시장분석 툴팁 추가

--- apps/user/src/components/UseTooltip.tsx
+++ apps/user/src/components/UseTooltip.tsx
@@ -4,30 +4,34 @@ import { Tooltip } from 'react-tooltip';
 interface IUseTooltip {
   content: ReactNode;
 }
-export const UseTooltip = ({ content }: IUseTooltip) => {
+const UseTooltip = ({ content }: IUseTooltip) => {
+  const randomKey = Math.random() * 100;
   return (
-    <div className='tooltip-container ml-0 xs:hidden'>
-      <a data-tooltip-id='anchor-keyword-search-volum'>
+    <div className='tooltip-container ml-4 xs:hidden'>
+      <a data-tooltip-id={`anchor-keyword-search-volum_${randomKey}`}>
         <ReactSVG
-          src='assets/icons/outlined/QuestionCircle.svg'
+          src='/assets/icons/outlined/QuestionCircle.svg'
           className=''
           beforeInjection={(svg) => {
-            svg.setAttribute('class', 'fill-grey-800 h-4 w-4 ');
+            svg.setAttribute('class', 'fill-grey-800 h-[18px] w-[18px]');
           }}
         />
       </a>
       <Tooltip
+        style={{ opacity: 1, background: 'content-box', border: 'none' }}
         render={() => {
           return (
-            <div className='rounded-lg border-[1px] border-grey-500 bg-grey-800 p-[15px] shadow-[0px_4px_8px_0px_rgba(0,0,0,0.15)]'>
-              {content}
+            <div className='rounded-lg bg-grey-800 p-[15px] shadow-[0px_4px_8px_0px_rgba(0,0,0,0.15)]'>
+              <div className='flex flex-col justify-start'>{content}</div>
             </div>
           );
         }}
-        id='anchor-keyword-search-volum'
+        id={`anchor-keyword-search-volum_${randomKey}`}
         place='right'
         variant='light'
       />
     </div>
   );
 };
+
+export default UseTooltip;
