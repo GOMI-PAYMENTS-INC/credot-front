3fb383bc317f8a2ddbe08e77221a0e337cf528d5
tate <jun931223@gmail.com>
2023-10-16 10:23:15 +0900

    fix: gnb router

--- apps/office/src/layouts/constants.ts
+++ apps/office/src/layouts/constants.ts
@@ -1,6 +1,6 @@
 import { PATH } from '@/common/constants';
 
 export const GNB_ROUTE = [
-  { text: '블로그', path: PATH.BLOG, icon: 'Preview', child: [PATH.CONTENT] },
+  { text: '블로그', path: PATH.BLOG, icon: 'Preview', child: [PATH.CATEGORY] },
   { text: '요금 안내', path: PATH.PRICE, icon: 'Coin', child: [] },
 ];
