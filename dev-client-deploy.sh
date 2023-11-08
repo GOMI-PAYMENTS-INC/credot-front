a88522f1d186a7de8aa00db18175487e0fe77ad4
inho <sayyou0918@gmail.com>
2023-11-08 15:56:53 +0900

    feat: 배포 스크립트 변경

--- dev-client-deploy.sh
+++ dev-client-deploy.sh
@@ -4,13 +4,21 @@ BRANCH=main
 
 cd /home/ec2-user/kr-gomi-settlement
 git reset --hard
-git pull origin $BRANCH
+git fetch --all
+git reset origin/$BRANCH --hard
 
 # front-api
 if [[ $(pm2 list | grep front-client) ]]; then
   pm2 restart front-client
 else
   pm2 start yarn --name "front-client" -- preview
+fi
+
+# office-api
+if [[ $(pm2 list | grep front-office) ]]; then
+  pm2 restart front-office
+else
+  pm2 start yarn --name "front-office" -- preview
 fi
 
 pm2 save
