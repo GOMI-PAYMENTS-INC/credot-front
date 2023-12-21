2fbbb136f38f60135bb8618c94f495cdfc902272
inho <sayyou0918@gmail.com>
2023-12-21 16:25:39 +0900

    feat: 미래정산 서비스 신청 추가

--- apps/office/src/v2/future-fund/components/DataTable.tsx
+++ apps/office/src/v2/future-fund/components/DataTable.tsx
@@ -1,11 +1,19 @@
-import { ConfigProvider, Table } from 'antd';
+import { Button, ConfigProvider, Input, InputNumber, Table } from 'antd';
 import { ColumnsType } from 'antd/es/table';
+import dayjs from 'dayjs';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 
+import { useUserById } from '@/hooks';
 import { FutureFundFilterAtom } from '@/v2/future-fund/atom';
 import { getDataTableColumns } from '@/v2/future-fund/components/DataTableColumns';
-import { useFutureFundList } from '@/v2/future-fund/hooks/future-fund.hook';
+import {
+  useFutureFundList,
+  useUpdatePrefundStatus,
+} from '@/v2/future-fund/hooks/future-fund.hook';
+import { useFutureFund } from '@/v2/prefund/hooks';
 
 const Wrapper = styled.div`
   .ant-table-thead .ant-table-cell,
@@ -31,18 +39,70 @@ export type FutureFundRecord = {
 };
 
 export const DataTable = () => {
+  const [applyPrice, setApplyPrice] = useState<number>(0);
   const [filter] = useRecoilState(FutureFundFilterAtom);
   const listColumn: ColumnsType<FutureFundRecord> = getDataTableColumns();
 
-  const { data, isInitialLoading: isLoading } = useFutureFundList({
+  const {
+    data,
+    isInitialLoading: isLoading,
+    refetch,
+  } = useFutureFundList({
     startAt: filter.termRange[0].format('YYYY-MM-DD'),
     endAt: filter.termRange[1].format('YYYY-MM-DD'),
     userId: filter.userId,
   });
+  const { refetch: refetchFutureFund } = useFutureFund(filter.userId);
+  const { mutateAsync } = useUpdatePrefundStatus();
+
+  const handleApplyFutureFund = async () => {
+    if (!filter.userId) {
+      toast.error('신청할 업체를 선택해주세요.');
+      return;
+    }
+
+    if (applyPrice <= 0) {
+      toast.error('신청할 금액을 입력하세요.');
+      return;
+    }
+
+    const today = dayjs().format('YYYY-MM-DD');
+    const res = await mutateAsync({
+      date: today,
+      userId: filter.userId,
+      price: applyPrice,
+    });
+    if (res) {
+      await refetch();
+      await refetchFutureFund();
+      setApplyPrice(0);
+    }
+  };
 
   return (
     <>
       <div className='mx-auto w-[1280px]'>
+        <div className='mt-[83px] flex flex-row justify-end'>
+          <div className='mr-[12px] self-center text-S/Medium text-grey-800'>
+            신청 금액
+          </div>
+          <div className='mr-[12px]'>
+            <InputNumber
+              placeholder='신청할 미래 정산 금액을 입력해주세요.'
+              suffix='원'
+              disabled={!filter.userId}
+              value={applyPrice}
+              onChange={(value) => setApplyPrice(value || 0)}
+              formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+              className='w-[260px]'
+            />
+          </div>
+          <div>
+            <Button disabled={!filter.userId} onClick={handleApplyFutureFund}>
+              등록
+            </Button>
+          </div>
+        </div>
         <Wrapper className='gm-h-full mt-[10px]'>
           <ConfigProvider
             theme={{
--- apps/office/src/v2/future-fund/hooks/future-fund.hook.ts
+++ apps/office/src/v2/future-fund/hooks/future-fund.hook.ts
@@ -1,6 +1,13 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
 
-import { FutureFundDto, FutureFundService } from '@/generated-rest/api/front';
+import {
+  ApplyFutureFundDto,
+  FutureFundDto,
+  FutureFundService,
+  PrefundService,
+  UpdatePrefundDto,
+} from '@/generated-rest/api/front';
 import { ApiError } from '@/generated-rest/api/front/core/ApiError';
 
 export const useFutureFundList = ({
@@ -17,4 +24,20 @@ export const useFutureFundList = ({
     queryFn: () => FutureFundService.list(startAt, endAt, Number(userId)),
     enabled: !!userId,
   });
+};
+
+export const useUpdatePrefundStatus = () => {
+  return useMutation(
+    (requestBody: ApplyFutureFundDto) => FutureFundService.apply(requestBody),
+    {
+      onSuccess: (res) => {
+        toast.success('미래 정산이 등록되었습니다.');
+        return res;
+      },
+      onError: (error: ApiError) => {
+        console.error(JSON.stringify(error));
+        toast.error('미래 정산이 등록이 실패했어요!');
+      },
+    },
+  );
 };
