29fc8d498e44dfb8dfc72b2a6a757d3feb1531e2
inho <sayyou0918@gmail.com>
2023-12-27 19:07:02 +0900

    feat: 미래 정산 현황 UI 모바일 수정

--- apps/user/src/v3/pages/home/components/FutureFundTable.tsx
+++ apps/user/src/v3/pages/home/components/FutureFundTable.tsx
@@ -24,72 +24,144 @@ export const MFutureFundTable = ({
 }: FutureFundTableType) => {
   return (
     <>
-      <Row className='rounded-[8px] bg-[#F2F2FF] py-[16px]'>
-        <Col span={12} className='text-center'>
+      <Row className='rounded-[8px] border border-grey-200'>
+        <Col
+          span={12}
+          className='rounded-bl-[8px] rounded-tl-[8px] bg-[#F2F2FF] py-[16px] text-center'
+        >
           사용중 금액
         </Col>
-        <Col span={12} className='text-center'>
-          신청 금액
+        <Col span={4} className='py-[16px] text-center' />
+        <Col
+          span={8}
+          className='rounded-br-[8px] rounded-tr-[8px] py-[16px] pr-[16px] text-right'
+        >
+          {localeString(futureFundPrice)}원
         </Col>
       </Row>
-      <Row className='mt-[14px] rounded-[8px] border border-grey-200 py-[16px]'>
-        <Col span={12} className='text-center'>
-          {localeString(futureFundPrice - applyPrice)}원
+
+      <Row className='mt-[12px] rounded-[8px] border border-grey-200'>
+        <Col
+          span={12}
+          className='rounded-bl-[8px] rounded-tl-[8px] bg-[#F2F2FF] py-[16px] text-center'
+        >
+          신청 금액
         </Col>
-        <Col span={12} className='text-center text-L/Medium'>
+        <Col span={4} className='py-[16px] text-center'>
+          <img src={plus} alt='+' className='mx-auto' />
+        </Col>
+        <Col
+          span={8}
+          className='rounded-br-[8px] rounded-tr-[8px] py-[16px] pr-[16px] text-right'
+        >
           {localeString(applyPrice)}원
         </Col>
       </Row>
 
-      <Row className='mt-[14px] rounded-[8px] bg-[#F2F2FF] py-[16px]'>
-        <Col span={12} className='text-center'>
+      <Row className='mt-[12px] rounded-[8px] border border-grey-200'>
+        <Col
+          span={12}
+          className='rounded-bl-[8px] rounded-tl-[8px] bg-[#F2F2FF] py-[16px] text-center'
+        >
           상환 금액
         </Col>
-        <Col span={12} className='text-center'>
-          상환 후 금액
+        <Col span={4} className='py-[16px] text-center'>
+          <img src={subtract} alt='-' className='mx-auto' />
         </Col>
-      </Row>
-      <Row className='mt-[14px] rounded-[8px] border border-grey-200 py-[16px]'>
-        <Col span={12} className='text-center'>
+        <Col
+          span={8}
+          className='rounded-br-[8px] rounded-tr-[8px] py-[16px] pr-[16px] text-right'
+        >
           {localeString(repaymentPrice)}원
         </Col>
-        <Col span={12} className='text-center'>
-          {localeString(futureFundPrice - applyPrice + repaymentPrice)}원
+      </Row>
+
+      <Row className='mt-[12px] rounded-[8px] border border-grey-200'>
+        <Col
+          span={12}
+          className='rounded-bl-[8px] rounded-tl-[8px] bg-[#F2F2FF] py-[16px] text-center'
+        >
+          상환 후 금액
+        </Col>
+        <Col span={4} className='py-[16px] text-center'>
+          <img src={equal} alt='=' className='mx-auto' />
+        </Col>
+        <Col
+          span={8}
+          className='rounded-br-[8px] rounded-tr-[8px] py-[16px] pr-[16px] text-right'
+        >
+          {localeString(futureFundPrice + applyPrice + repaymentPrice)}원
         </Col>
       </Row>
 
       <Divider />
 
-      <Row className='mt-[14px] rounded-[8px] bg-[#F2F2FF] py-[16px]'>
-        <Col span={12} className='text-center'>
+      <Row className='mt-[12px] rounded-[8px] border border-grey-200'>
+        <Col
+          span={12}
+          className='rounded-bl-[8px] rounded-tl-[8px] bg-[#F2F2FF] py-[16px] text-center'
+        >
           미납 수수료
         </Col>
-        <Col span={12} className='text-center'>
-          발생 수수료
-        </Col>
-      </Row>
-      <Row className='mt-[14px] rounded-[8px] border border-grey-200 py-[16px]'>
-        <Col span={12} className='text-center'>
+        <Col span={4} className='py-[16px] text-center'></Col>
+        <Col
+          span={8}
+          className='rounded-br-[8px] rounded-tr-[8px] py-[16px] pr-[16px] text-right'
+        >
           {localeString(accumulatedFees - accrualFees)}원
         </Col>
-        <Col span={12} className='text-center'>
+      </Row>
+
+      <Row className='mt-[12px] rounded-[8px] border border-grey-200'>
+        <Col
+          span={12}
+          className='rounded-bl-[8px] rounded-tl-[8px] bg-[#F2F2FF] py-[16px] text-center'
+        >
+          발생 수수료
+        </Col>
+        <Col span={4} className='py-[16px] text-center'>
+          <img src={plus} alt='+' className='mx-auto' />
+        </Col>
+        <Col
+          span={8}
+          className='rounded-br-[8px] rounded-tr-[8px] py-[16px] pr-[16px] text-right'
+        >
           {localeString(accrualFees)}원
         </Col>
       </Row>
 
-      <Row className='mt-[14px] rounded-[8px] bg-[#F2F2FF] py-[16px]'>
-        <Col span={12} className='text-center'>
+      <Row className='mt-[12px] rounded-[8px] border border-grey-200'>
+        <Col
+          span={12}
+          className='rounded-bl-[8px] rounded-tl-[8px] bg-[#F2F2FF] py-[16px] text-center'
+        >
           납부한 수수료
         </Col>
-        <Col span={12} className='text-center'>
-          납부할 수수료
+        <Col span={4} className='py-[16px] text-center'>
+          <img src={subtract} alt='-' className='mx-auto' />
         </Col>
-      </Row>
-      <Row className='mt-[14px] rounded-[8px] border border-grey-200 py-[16px]'>
-        <Col span={12} className='text-center'>
+        <Col
+          span={8}
+          className='rounded-br-[8px] rounded-tr-[8px] py-[16px] pr-[16px] text-right'
+        >
           {localeString(repaymentFees)}원
         </Col>
-        <Col span={12} className='text-center'>
+      </Row>
+
+      <Row className='mt-[12px] rounded-[8px] border border-grey-200'>
+        <Col
+          span={12}
+          className='rounded-bl-[8px] rounded-tl-[8px] bg-[#F2F2FF] py-[16px] text-center'
+        >
+          납부할 수수료
+        </Col>
+        <Col span={4} className='py-[16px] text-center'>
+          <img src={equal} alt='=' className='mx-auto' />
+        </Col>
+        <Col
+          span={8}
+          className='rounded-br-[8px] rounded-tr-[8px] py-[16px] pr-[16px] text-right'
+        >
           {localeString(accumulatedFees + repaymentFees)}원
         </Col>
       </Row>
@@ -126,7 +198,7 @@ export const PCFutureFundTable = ({
       </Row>
       <Row className='mt-[14px] rounded-[8px] border border-grey-200 py-[16px]'>
         <Col span={5} className='text-center'>
-          {localeString(futureFundPrice - applyPrice)}원
+          {localeString(futureFundPrice)}원
         </Col>
         <Col span={1} className='self-center text-center'>
           <img src={plus} alt='+' />
@@ -144,7 +216,7 @@ export const PCFutureFundTable = ({
           <img src={equal} alt='=' />
         </Col>
         <Col span={5} className='text-center'>
-          {localeString(futureFundPrice - applyPrice + repaymentPrice)}원
+          {localeString(futureFundPrice + applyPrice + repaymentPrice)}원
         </Col>
       </Row>
       <Row className='mt-[14px] rounded-[8px] bg-[#F2F2FF] py-[16px]'>
@@ -193,7 +265,7 @@ export const PCFutureFundTable = ({
 
 export const FutureFundTable = () => {
   const { data: futureFund } = useTodayFutureFundHook();
-  if (futureFund?.futureFundPrice === 0) {
+  if (!futureFund) {
     return (
       <>
         <Row
