8f8a642bca8c11725ab63754e708370bd1c954d8
inho <sayyou0918@gmail.com>
2024-01-04 22:08:49 +0900

    feat: 수정

--- apps/user/src/auth/signIn/SignIn.tsx
+++ apps/user/src/auth/signIn/SignIn.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { isMobile } from 'react-device-detect';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -6,6 +7,7 @@ import { _amplitudeLoginPageViewed } from '@/amplitude/amplitude.service';
 import { NOTIFICATION_MESSAGE } from '@/auth/constants';
 import { useLoginHook } from '@/auth/hooks/login.hook';
 import { PATH } from '@/common/constants';
+import { Common2Section } from '@/common/layouts';
 import { InputIcon, INPUTSTATUS } from '@/components/InputIcon';
 import { LoginDto } from '@/generated-rest/api/front';
 import { authTokenStorage } from '@/utils/authToken';
@@ -15,7 +17,7 @@ interface ISignInForm {
   password: string;
 }
 
-export const SignIn = () => {
+const Login = () => {
   const navigation = useNavigate();
   const { mutate: loginMutate } = useLoginHook();
 
@@ -66,134 +68,144 @@ export const SignIn = () => {
   };
 
   return (
-    <Default sideBar={false}>
-      <div className='px-[20px]'>
-        <div>
-          <h3 className='text-center text-3XL/Medium md:hidden'>로그인</h3>
-        </div>
-        <div className='mt-10 space-y-12 md:mt-[56px] md:space-y-8'>
-          <form onSubmit={handleSubmit(onValid)}>
-            <div className='space-y-4'>
-              <div className='space-y-8'>
+    <div className='px-[20px]'>
+      <div>
+        <h3 className='text-center text-3XL/Medium md:hidden'>로그인</h3>
+      </div>
+      <div className='mt-10 space-y-12 md:mt-[56px] md:space-y-8'>
+        <form onSubmit={handleSubmit(onValid)}>
+          <div className='space-y-4'>
+            <div className='space-y-8'>
+              <div className='inputCustom-group'>
+                <label className='inputCustom-label'>이메일</label>
+                <div className='inputCustom-textbox-wrap'>
+                  <input
+                    className={`inputCustom-textbox w-full ${
+                      errors?.email ? 'error' : ''
+                    }`}
+                    type='email'
+                    placeholder='이메일'
+                    {...register('email', {
+                      required: NOTIFICATION_MESSAGE.emptyEmail,
+                      pattern: {
+                        value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                        message: NOTIFICATION_MESSAGE.invalidEmail,
+                      },
+                    })}
+                  />
+                  <InputIcon
+                    status={errors?.email ? INPUTSTATUS.ERROR : undefined}
+                    iconSize={5}
+                  />
+                </div>
+                {errors?.email?.message && (
+                  <p className='inputCustom-helptext'>{errors?.email?.message}</p>
+                )}
+              </div>
+              <div className='space-y-2'>
                 <div className='inputCustom-group'>
-                  <label className='inputCustom-label'>이메일</label>
+                  <label className='inputCustom-label'>비밀번호</label>
                   <div className='inputCustom-textbox-wrap'>
                     <input
                       className={`inputCustom-textbox w-full ${
-                        errors?.email ? 'error' : ''
+                        errors?.password ? 'error' : ''
                       }`}
-                      type='email'
-                      placeholder='이메일'
-                      {...register('email', {
-                        required: NOTIFICATION_MESSAGE.emptyEmail,
+                      type='password'
+                      placeholder='비밀번호를 입력해주세요. (8자리 이상)'
+                      {...register('password', {
+                        required: '비밀번호를 입력해주세요.',
                         pattern: {
-                          value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-                          message: NOTIFICATION_MESSAGE.invalidEmail,
+                          // : 모든 글자 8자리 이상 입력
+                          value: /^.{8,}$/,
+                          message: '비밀번호는 8자리 이상 입력해주세요.',
                         },
                       })}
                     />
                     <InputIcon
-                      status={errors?.email ? INPUTSTATUS.ERROR : undefined}
+                      status={errors?.password ? INPUTSTATUS.ERROR : undefined}
                       iconSize={5}
                     />
                   </div>
-                  {errors?.email?.message && (
-                    <p className='inputCustom-helptext'>{errors?.email?.message}</p>
+                  {errors?.password?.message && (
+                    <p className='inputCustom-helptext'>{errors?.password?.message}</p>
                   )}
                 </div>
-                <div className='space-y-2'>
-                  <div className='inputCustom-group'>
-                    <label className='inputCustom-label'>비밀번호</label>
-                    <div className='inputCustom-textbox-wrap'>
-                      <input
-                        className={`inputCustom-textbox w-full ${
-                          errors?.password ? 'error' : ''
-                        }`}
-                        type='password'
-                        placeholder='비밀번호를 입력해주세요. (8자리 이상)'
-                        {...register('password', {
-                          required: '비밀번호를 입력해주세요.',
-                          pattern: {
-                            // : 모든 글자 8자리 이상 입력
-                            value: /^.{8,}$/,
-                            message: '비밀번호는 8자리 이상 입력해주세요.',
-                          },
-                        })}
-                      />
-                      <InputIcon
-                        status={errors?.password ? INPUTSTATUS.ERROR : undefined}
-                        iconSize={5}
-                      />
-                    </div>
-                    {errors?.password?.message && (
-                      <p className='inputCustom-helptext'>{errors?.password?.message}</p>
-                    )}
-                  </div>
-                </div>
-              </div>
-              <div className='flex items-center justify-between'>
-                <div className='flex items-center'>
-                  <input
-                    id='remember_me'
-                    name='remember_me'
-                    type='checkbox'
-                    className='checkboxCustom peer'
-                  />
-                  <label
-                    htmlFor='remember_me'
-                    className='checkboxCustom-label bg-[length:24px_24px] bg-[left_top_50%] pl-[30px] text-S/Regular  '
-                  >
-                    로그인 상태 유지
-                  </label>
-                </div>
-                <div>
-                  <button
-                    type='button'
-                    onClick={() => navigation(PATH.FIND_ID)}
-                    className='cursor-pointer justify-center  bg-white py-2.5  px-2 text-S/Bold text-grey-800'
-                  >
-                    아이디 찾기
-                  </button>
-
-                  <button
-                    type='button'
-                    onClick={() => navigation(PATH.FIND_PASSWORD)}
-                    className='cursor-pointer justify-center   bg-white py-2.5  px-2 text-S/Bold text-grey-800'
-                  >
-                    비밀번호 찾기
-                  </button>
-                </div>
-              </div>
-
-              <div className='space-y-3'>
-                <div>
-                  <button
-                    type='submit'
-                    className='button-filled-normal-xLarge-red-false-false-true  w-full '
-                  >
-                    로그인
-                  </button>
-                </div>
               </div>
             </div>
-          </form>
-          <div className='space-y-4'>
-            <div className='text-center text-M/Regular text-grey-700'>
-              아직 회원이 아니세요?
+            <div className='flex items-center justify-between'>
+              <div className='flex items-center'>
+                <input
+                  id='remember_me'
+                  name='remember_me'
+                  type='checkbox'
+                  className='checkboxCustom peer'
+                />
+                <label
+                  htmlFor='remember_me'
+                  className='checkboxCustom-label bg-[length:24px_24px] bg-[left_top_50%] pl-[30px] text-S/Regular  '
+                >
+                  로그인 상태 유지
+                </label>
+              </div>
+              <div>
+                <button
+                  type='button'
+                  onClick={() => navigation(PATH.FIND_ID)}
+                  className='cursor-pointer justify-center  bg-white py-2.5  px-2 text-S/Bold text-grey-800'
+                >
+                  아이디 찾기
+                </button>
+
+                <button
+                  type='button'
+                  onClick={() => navigation(PATH.FIND_PASSWORD)}
+                  className='cursor-pointer justify-center   bg-white py-2.5  px-2 text-S/Bold text-grey-800'
+                >
+                  비밀번호 찾기
+                </button>
+              </div>
             </div>
-            <div>
-              <Link to={PATH.SIGN_UP}>
+
+            <div className='space-y-3'>
+              <div>
                 <button
                   type='submit'
-                  className='button-outlined-normal-xLarge-primary-false-false-true w-full md:mb-[114px]'
+                  className='button-filled-normal-xLarge-red-false-false-true  w-full '
                 >
-                  회원가입 하기
+                  로그인
                 </button>
-              </Link>
+              </div>
             </div>
+          </div>
+        </form>
+        <div className='space-y-4'>
+          <div className='text-center text-M/Regular text-grey-700'>
+            아직 회원이 아니세요?
+          </div>
+          <div>
+            <Link to={PATH.SIGN_UP}>
+              <button
+                type='submit'
+                className='button-outlined-normal-xLarge-primary-false-false-true w-full md:mb-[114px]'
+              >
+                회원가입 하기
+              </button>
+            </Link>
           </div>
         </div>
       </div>
+    </div>
+  );
+};
+
+export const SignIn = () => {
+  return isMobile ? (
+    <Default sideBar={false}>
+      <Login />
     </Default>
+  ) : (
+    <Common2Section>
+      <Login />
+    </Common2Section>
   );
 };
