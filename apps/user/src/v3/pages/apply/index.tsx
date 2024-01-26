f012e18565f0fbe68d337b117841f120b9897cf3
inho <sayyou0918@gmail.com>
2024-01-26 13:10:44 +0900

    feat: 도입문의 홈 이동

--- apps/user/src/v3/pages/apply/index.tsx
+++ apps/user/src/v3/pages/apply/index.tsx
@@ -1,5 +1,6 @@
 import { Layout } from 'antd';
 import { isMobile } from 'react-device-detect';
+import { useNavigate } from 'react-router-dom';
 import { ReactSVG } from 'react-svg';
 
 import { Default } from '@/v3/layouts';
@@ -8,10 +9,14 @@ import { ApplyFormCard, MApplyFormCard } from '@/v3/pages/apply/components';
 const { Header, Content } = Layout;
 
 function PCApply() {
+  const navigate = useNavigate();
   return (
     <Layout className='bg-transparent'>
       <Header className='h-auto border-b border-grey-300 bg-white py-[24px]'>
-        <div className='mx-auto flex  w-[1100px] items-center justify-center'>
+        <div
+          className='mx-auto flex  w-[1100px] cursor-pointer items-center justify-center'
+          onClick={() => navigate('/')}
+        >
           <ReactSVG
             src='/assets/en-logo.svg'
             beforeInjection={(svg) => {
