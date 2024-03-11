6eabf17d6311d5d36163baf5f3a9a35c82d66eb5
inho <sayyou0918@gmail.com>
2024-03-11 17:40:04 +0900

    chore: 선정산 재요청 버튼

--- apps/office/src/v2/prefund/components/header.tsx
+++ apps/office/src/v2/prefund/components/header.tsx
@@ -1,7 +1,17 @@
-export const Header = ({ title }: { title: string; updateAt: string }) => {
+import { ReactNode } from 'react';
+
+export const Header = ({
+  title,
+  children,
+}: {
+  title: string;
+  updateAt: string;
+  children?: ReactNode;
+}) => {
   return (
     <div className='mx-auto flex w-[1280px] items-center'>
       <div className='mr-[20px] text-XL/Bold text-grey-900'>{title}</div>
+      <div>{children}</div>
     </div>
   );
 };
--- apps/office/src/v2/prefund/withdrawal-ready.tsx
+++ apps/office/src/v2/prefund/withdrawal-ready.tsx
@@ -1,10 +1,12 @@
-import { Button, Modal } from 'antd';
+import { Button, message, Modal } from 'antd';
 import dayjs from 'dayjs';
 import { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 
 import { Default } from '@/common/layouts';
 import { PrefundStatusEnum } from '@/generated-rest/api/front';
+import { OpenAPI } from '@/generated-rest/api/front/core/OpenAPI';
+import { request as __request } from '@/generated-rest/api/front/core/request';
 import { usePrefundList } from '@/hooks/prefund.hook';
 import { PrefundFilterAtom } from '@/v2/prefund/atom';
 import { DataTable } from '@/v2/prefund/components/DataTable';
@@ -16,7 +18,7 @@ import { UserInfoSection } from '@/v2/prefund/components/UserInfoSection';
 
 export const WithdrawalReady = () => {
   const [filter, setFilter] = useRecoilState(PrefundFilterAtom);
-  const { dataUpdatedAt } = usePrefundList({
+  const { dataUpdatedAt, refetch } = usePrefundList({
     status: PrefundStatusEnum.READY,
     startAt: filter.termRange[0].format('YYYY-MM-DD'),
     endAt: filter.termRange[1].format('YYYY-MM-DD'),
@@ -36,7 +38,37 @@ export const WithdrawalReady = () => {
       <Header
         title='선정산 지급하기'
         updateAt={dayjs(dataUpdatedAt).format('YYYY-MM-DD HH:mm:ss')}
-      />
+      >
+        <Button
+          onClick={() =>
+            Modal.confirm({
+              title: '선정산 요청',
+              content: '선정산 내역을 다시 요청하시겠습니까?',
+              okText: '요청하기',
+              cancelText: '닫기',
+              onOk: async () => {
+                await __request(OpenAPI, {
+                  method: 'GET',
+                  url: '/request/daily/prefund',
+                  query: {
+                    target: filter.termRange[0].format('YYYY-MM-DD'),
+                  },
+                })
+                  .then(() => {
+                    message.success('성공적으로 선정산을 요청했어요.');
+                    refetch();
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                    message.error('선정산을 실패했어요.');
+                  });
+              },
+            })
+          }
+        >
+          선정산 요청
+        </Button>
+      </Header>
       <Filter dateRangeOn={false} dataFilterCriteriaLabel={undefined} />
       {filter.userId && (
         <>
